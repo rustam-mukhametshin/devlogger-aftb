b68ba4612dd23bc6bbf2d8e50138c0d593164c3a
iproman <itechman@yandex.ru>
2019-12-27 21:09:32 +0500

    Add submit form function and generating id.

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -12,17 +12,50 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean;
+
   constructor(private logService: LogService) {
   }
 
   ngOnInit() {
+    this.isNew = true;
+
     // Subscribe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
+        this.isNew = false;
       }
+    });
+  }
+
+  onSubmit() {
+
+    // Check if new log
+    if (this.isNew) {
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      this.logService.addLog(newLog);
+    } else {
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 
